ff8cbb5a9f467983b69ccdf5106af2143bdc56dc
WooSangWook <raincoat@kakao.com>
2023-03-02 16:06:05 +0900

    Feat: swagger default settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,17 @@
 import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/common/swagger.config';
 
 async function bootstrap() {
   config();
+
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(3000);
+
+  console.info('Server is running on port 3000');
 }
 bootstrap();
