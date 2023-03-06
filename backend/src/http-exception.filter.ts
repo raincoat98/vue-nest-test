5c62e0ec29806918041d2d09ffacd54f41d11a4b
WooSangWook <raincoat@kakao.com>
2023-03-06 21:43:35 +0900

    Feat: exception filter

---
+++ backend/src/http-exception.filter.ts
@@ -0,0 +1,36 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const error = exception.getResponse() as
+      | string
+      | { message: string; statusCode: number; error: string | string[] };
+
+    if (typeof error === 'string') {
+      response.status(status).json({
+        scuccess: false,
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        error,
+      });
+    } else {
+      response.status(status).json({
+        scuccess: false,
+        timestamp: new Date().toISOString(),
+        ...error,
+      });
+    }
+  }
+}
