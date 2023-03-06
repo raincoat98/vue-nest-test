e47bf3acfd7ba566a8e6910bef345ee6de43c348
WooSangWook <raincoat@kakao.com>
2023-03-06 12:25:49 +0900

    Feat: default cloud settings

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,38 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const path = require("path");
+console.log(__dirname);
+
+module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html", // source template
+      filename: "index.html", // output as dist/index.html
+      title: "Index Page", // when using title option,
+    },
+  },
+  outputDir: path.join(__dirname, "..", "./backend/front"),
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias.set("@", path.resolve(__dirname, "src/"));
+  },
+  css: {
+    extract: {
+      filename:
+        "filname" + "-" + require("./package.json").version + ".min.css",
+    },
+  },
+  configureWebpack: {
+    output: {
+      filename: "filname" + "-" + require("./package.json").version + ".min.js",
+    },
+    optimization: {
+      splitChunks: false,
+    },
+  },
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+};
