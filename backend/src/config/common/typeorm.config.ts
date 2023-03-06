d4b34327f500407163c30bf27a476227ec80de8d
WooSangWook <raincoat@kakao.com>
2023-03-06 17:23:46 +0900

    Feat: typeorm settings

---
+++ backend/src/config/common/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const getTypeOrmConfig = async (
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> => {
+  return {
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USER'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true,
+    autoLoadEntities: true,
+  };
+};
