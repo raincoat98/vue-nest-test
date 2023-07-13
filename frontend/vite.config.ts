134d8efcd83d594ad11df3fc9a8de6c152ce9b6e
WooSangWook <raincoat@kakao.com>
2023-07-13 15:15:06 +0900

    Feat: 빌드 경로 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve, join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 8001,
+    open: true, // 브라우저 자동 실행
   },
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+  },
+  build: {
+    outDir: join(__dirname, "..", "./backend/front"), // 빌드 파일 설치 경로
+    rollupOptions: {},
   },
   plugins: [vue()],
 });
