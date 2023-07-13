0c66a0e9d9fdc5943fc6f1a0ba0b7ff7f1888b32
WooSangWook <raincoat@kakao.com>
2023-07-13 15:55:57 +0900

    Feat: front end setting reset

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve, join } from "path";
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   server: {
     port: 8001,
-    open: true, // 브라우저 자동 실행
+    open: true // 브라우저 자동 실행
   },
+
   resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   build: {
-    outDir: join(__dirname, "..", "./backend/front"), // 빌드 파일 설치 경로
-    rollupOptions: {},
-  },
-  plugins: [vue()],
-});
+    outDir: join(__dirname, '..', './backend/front'), // 빌드 파일 설치 경로
+    rollupOptions: {}
+  }
+})
